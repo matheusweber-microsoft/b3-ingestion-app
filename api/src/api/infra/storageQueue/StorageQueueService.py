c56f4a027408c604a8cff61220bdbdcc092cd38b
Matheus Weber <matheusweber@microsoft.com>
2024-05-08 18:32:25 -0300

    Fix the bug

--- api/src/api/infra/storageQueue/StorageQueueService.py
+++ api/src/api/infra/storageQueue/StorageQueueService.py
@@ -1,5 +1,7 @@
 from api.infra.secrets import Secrets
 from azure.storage.queue import QueueClient
+import json
+import base64
 
 class StorageQueueService:
     def __init__(self, queue_name):
@@ -8,5 +10,8 @@ class StorageQueueService:
         self.queue_client = QueueClient.from_connection_string(secrets.connection_string_azure_queue, queue_name)
 
     def send_message(self, message_dict):
-        message = str(message_dict)  # Convert the dictionary to a string
-        self.queue_client.send_message(message)
+        message = json.dumps(message_dict)
+        message_bytes = message.encode('utf-8')  
+        base64_bytes = base64.b64encode(message_bytes) 
+        base64_message = base64_bytes.decode('utf-8')  
+        self.queue_client.send_message(base64_message)
