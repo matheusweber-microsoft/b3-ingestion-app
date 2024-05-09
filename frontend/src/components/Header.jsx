e5be851657ceb7eda85d58be54d023f963304a4b
Matheus Weber <matheusweber@microsoft.com>
2024-05-09 18:47:16 -0300

    Start

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+<<<<<<< HEAD
 import lightTheme from "../styles/theme.js";
 
 export default function Header() {
@@ -11,3 +12,19 @@ export default function Header() {
     </header>
   );
 }
+=======
+import lightTheme from '../styles/theme.js';
+
+export default function Header() {
+    return (
+        <header>
+            <div className="header h-6 pt-1">
+                <div className="user-info float-right text-xs pr-15" style={{ paddingRight: '15px' }}>
+                    <span className="username">Welcome Jade@b3.com.br</span>
+                </div>
+            </div>
+            <div className="divider bg-gray-300 h-0.5"></div>
+        </header>
+    );
+}
+>>>>>>> f06a479 (Start)
