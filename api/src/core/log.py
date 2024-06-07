50aa1baf4681920d787d4017dd95964a5c5468ec
Matheus Weber <matheusweber@microsoft.com>
2024-06-07 15:12:18 -0300

    Remove key vault and managed identiy

--- api/src/core/log.py
+++ api/src/core/log.py
@@ -1,19 +1,17 @@
 import logging
 import os
-
-from src.infra.keyVault.keyVault import KeyVault
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 class Logger:
     def __init__(self):
-        keyVault = KeyVault()
         self.logger = logging.getLogger()
         if not self.logger.handlers:
             log_level = os.getenv('LOG_LEVEL', 'INFO')
             self.logger.setLevel(getattr(logging, log_level))
             
             # Azure log handler
-            azure_handler = AzureLogHandler(connection_string=keyVault.get_secret(os.getenv('KEY_VAULT_APPLICATION_INSIGHTS_NAME')))
+            azure_log_handler_connection_string = os.getenv('APP-INSIGHTS-CONN-STRING')
+            azure_handler = AzureLogHandler(connection_string=azure_log_handler_connection_string)
             formatter = logging.Formatter('%(filename)s - %(levelname)s - %(message)s')
             azure_handler.setFormatter(formatter)
             self.logger.addHandler(azure_handler)
